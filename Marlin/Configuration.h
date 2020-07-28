d4034659c98f217f7d2dfdcd5fe07f0dadb7fc11
Graham-3D <42432546+Graham-3D@users.noreply.github.com>
2020-07-28 19:29:19 +0100

    Update Configuration.h
    Change runout state again. Seems to work. On second print.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1170,9 +1170,9 @@
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define NUM_RUNOUT_SENSORS   1     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
   //#define FIL_RUNOUT_INVERTING true // set to true to invert the logic of the sensor.
-  #define FIL_RUNOUT_STATE    LOW  // LOW works 28072020 Pin state indicating that filament is NOT present.
-  #define FIL_RUNOUT_PULLUP          // works 28072020 Use internal pullup for filament runout pins.
-  //#define FIL_RUNOUT_PULLDOWN      // Use internal pulldown for filament runout pins.
+  #define FIL_RUNOUT_STATE   HIGH   // HIGH works 28072020 Pin state indicating that filament is NOT present.
+  //#define FIL_RUNOUT_PULLUP          // Use internal pullup for filament runout pins.
+  #define FIL_RUNOUT_PULLDOWN      // works 28072020 Use internal pulldown for filament runout pins.
 
   // Set one or more commands to execute on filament runout.
   // (After 'M412 H' Marlin will ask the host to handle the process.)
