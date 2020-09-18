710eefdc6f3d6a0c07245969b305f6efaf7d4b31
Graham-3D <42432546+Graham-3D@users.noreply.github.com>
2020-09-18 21:53:25 +0100

    fix babystep
    Fix babystep

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -991,7 +991,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -23, 10, -1.96 }
+#define NOZZLE_TO_PROBE_OFFSET { -23, 10, -1.42 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
